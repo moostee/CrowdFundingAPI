c1d0c2b5e0870be08cdd53d6d3abe7a165159279
Mustapha Omotunwase <mustapha.omotunwase@ubagroup.com>
2020-01-16 18:12:57 +0100

    jwt added and tested ok

--- src/Utility/jwt.py
+++ src/Utility/jwt.py
@@ -1,21 +1,11 @@
 import jwt
-import os
-import environ
-from datetime import datetime,timedelta
-
-env = environ.Env()
-environ.Env.read_env()
 
 class Jwt:
 
     @staticmethod
-    def EncodeJWT(payload, secret, theAlgorithm='HS256'):       
-        if env('expiryInSeconds') is not None :
-            secondItExpires = int(env('expiryInSeconds'))
-            setattr(payload, 'exp', datetime.utcnow() + timedelta.seconds(seconds=secondItExpiries))
-
+    def EncodeJWT(payload, secret, theAlgorithm='HS256'):  
         return jwt.encode(payload, secret, algorithm=theAlgorithm)
 
     @staticmethod
-    def DecodeJWT(encodedJWT,theAlgorithm=['HS256']):
+    def DecodeJWT(encodedJWT, secret, theAlgorithm=['HS256']):
         return jwt.decode(encodedJWT, secret, algorithm=theAlgorithm)
