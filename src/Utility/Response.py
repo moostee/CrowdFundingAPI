83fededc0d15214cf7abd05bd144bfef02b61ea8
frankly034 <franklynugege@gmail.com>
2020-01-16 14:41:30 +0100

    bug(response): fix response wrapper
    - remove rest_framework.response from Response wrapper class

--- src/Utility/Response.py
+++ src/Utility/Response.py
@@ -1,24 +1,20 @@
-from rest_framework.response import Response as response
-
 class Response:
     ''' A Response class wrapper for all HTTP responses'''
     
     @staticmethod
-    def success(message='success', data=None, responseCode='00',  headers=None, content_type='application/json'):
+    def success(message='success', data=None, responseCode='00'):
         """Returns a success HTTP response"""
-        responseData = {
+        return {
             "message": message,
             "responseCode": responseCode,
             "data": data,
         }
-        return response(data=responseData, headers=headers, content_type=content_type)
 
     @staticmethod
-    def error(message='Error occured while processing your request', error=None, responseCode='99', headers=None, content_type='application/json'):
+    def error(message='Error occured while processing your request', error=None, responseCode='99'):
         """Returns a failure HTTP response"""
-        responseData = {
+        return {
             "message": message,
             "responseCode": responseCode,
             "error": error
         }
-        return response(data=responseData, headers=headers, content_type=content_type)
