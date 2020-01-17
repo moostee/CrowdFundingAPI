a99ba0da23fe044e285beea025ab11214d4be10d
frankly034 <franklynugege@gmail.com>
2020-01-17 17:33:17 +0100

    bug(resource): fix base resource endpoint
    - add base resource endpoint
    - remove __pycache__ folder

--- src/ControllerLayer/default/viewsets.py
+++ src/ControllerLayer/default/viewsets.py
@@ -1,7 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from Utility.Response import Response as response
+from drf_yasg.utils import swagger_auto_schema
 
 class DefaultViewSet(APIView):
+    @swagger_auto_schema(operation_description="API Welcome View", responses={200: '{"Message":"Welcome crowdfunding api"}'})
     def get(self, request, format=None):
         return Response(response.success(data={"Message":"Welcome crowdfunding api"}))
