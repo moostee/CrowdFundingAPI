b0e080b57bc2ba0a2f50d07acd764fe0a43591a5
frankly034 <franklynugege@gmail.com>
2020-01-21 07:48:37 +0100

    bug(response): add requestid to base route
    - add request id to base route
    - add logging to base route
    - delte Pipfile from the root of the app

--- src/ControllerLayer/default/viewsets.py
+++ src/ControllerLayer/default/viewsets.py
@@ -2,8 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from Utility.Response import Response as response
 from drf_yasg.utils import swagger_auto_schema
+from Utility.logger import Logger
+import uuid
 
 class DefaultViewSet(APIView):
     @swagger_auto_schema(operation_description="API Welcome View", responses={200: '{"Message":"Welcome crowdfunding api"}'})
     def get(self, request, format=None):
-        return Response(response.success(data={"Message":"Welcome crowdfunding api"}))
+        logger = Logger('ControllerLayer.Default')
+        requestId = uuid.uuid4()
+        logger.Info(r"Default Route was called. \n REQUESTID => {}".format(requestId))
+        return Response(response.success(requestId, data={"Message":"Welcome crowdfunding api"}))
