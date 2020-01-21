d769b15487e4b030bfcbd3805d9502f295bc413f
frankly034 <franklynugege@gmail.com>
2020-01-21 15:20:53 +0100

    bug(middlware): fix validate user role middlware
    - add requestId
    - change the request access property

--- src/Utility/middlewares/validateUserRole.py
+++ src/Utility/middlewares/validateUserRole.py
@@ -1,16 +1,18 @@
 from rest_framework.response import Response
 from rest_framework import status
 from ..Response import Response as ResponseWrapper
+import uuid
 
 class ValidateUserRole:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        userRole = request.authUser['RoleName']
+        requestId = uuid.uuid4()
+        userRole = request.authUser['roleName']
 
         if userRole == 'Admin':
             response = self.get_response(request)
             return response
         else:
-            return Response(ResponseWrapper.error(message='Access Denied. You lack the permission to access this route'), status.HTTP_401_UNAUTHORIZED)
+            return Response(ResponseWrapper.error(requestId, message='Access Denied. You lack the permission to access this route'), status.HTTP_401_UNAUTHORIZED)
