8d57fb2b39330c3fc98f7e08c5beaa42158e2d4b
frankly034 <franklynugege@gmail.com>
2020-01-29 12:41:48 +0100

    bug(resource): fix signup based on auth type
    - add signup signupSerializer
    - refactor user viewsets
    - refactor user service

--- src/DataAccessLayer/User/repository.py
+++ src/DataAccessLayer/User/repository.py
@@ -5,3 +5,6 @@ class UserRepository(BaseRepository):
 
     def getByUserId(self, value):
         return self.model.objects.filter(userId=value, isDeleted=False)
+
+    def checkPhoneNumberExist(self, value):
+        return self.model.objects.filter(phoneNumber=value, isDeleted=False).exists()
