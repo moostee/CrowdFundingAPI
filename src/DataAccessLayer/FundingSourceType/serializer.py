b3ae85c7b1d7a2fa78df13b7a45df37e0359da89
Mustapha Omotunwase <mustapha.omotunwase@ubagroup.com>
2020-02-06 11:52:30 +0100

    config serializer modified to accept rules of key/value pair

--- src/DataAccessLayer/FundingSourceType/serializer.py
+++ src/DataAccessLayer/FundingSourceType/serializer.py
@@ -1,13 +1,7 @@
 from rest_framework import serializers
 from ..FundingSourceType.model import FundingSourceType
 from ..DynamicSerializer import DynamicFieldsModelSerializer
-
-class ConfigSerializer(serializers.Serializer):
-    field = serializers.CharField(max_length=50,allow_blank=False)
-    label = serializers.CharField(max_length=50,allow_blank=False)
-    type = serializers.CharField(max_length=50,allow_blank=False)
-    tip = serializers.CharField(max_length=50,allow_blank=False)
-    rules = serializers.DictField(allow_empty=False)
+from ..ConfigSerializer import ConfigSerializer
 
 class FundingSourceTypeSerializer(DynamicFieldsModelSerializer):
     config = ConfigSerializer(many=True,allow_empty=False)
